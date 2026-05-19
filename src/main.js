70bfa38ea585ae5d9c13914ce4fe51d50c26ff65
k1950035 <53138789+k1950035@users.noreply.github.com>
2026-05-19 15:43:23 +0900

    Week 10 : Pinia 및 로컬 스토리지 연동 완료

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import './assets/main.css'
-
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+// 앱에 라우터와 피니아(중앙 금고)를 장착
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
